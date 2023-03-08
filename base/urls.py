0cb8a5049f54b1bce98cde86b7d08f7921c6aac5
OliverRekky <bpnguyenduyanh@gmail.com>
2023-03-08 10:12:02 +0700

    upload studybud

---
+++ base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+	path('', views.home, name='home'),
+	path('room/<str:pk>/', views.room, name='room'),
+	path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+	path('create-room/', views.create_room, name='create-room'),
+	path('update-room/<str:pk>/', views.update_room, name='update-room'),
+	path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+	path('login/', views.loginPage, name='login'),
+	path('logout/', views.logoutUser, name='logout'),
+	path('register/', views.registerPage, name='register'),
+	path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
+	path('update-user/', views.update_user, name='update-user'),
+]
